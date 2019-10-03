275831b0aa1c231f023eddb9f6ea384d40b55da6
Colcothar <33038729+Colcothar@users.noreply.github.com>
2019-10-03 18:25:24 +0100

    Update test.py

--- Data Logger/test.py
+++ Data Logger/test.py
@@ -3,10 +3,10 @@ import Adafruit_DHT
 from time import sleep
 
 while True:
-	data = open ("data.txt", "a")
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
-	data.write('Temp={0:0.1f}*  Humidity={1:0.1f}% \n'.format(temperature, humidity))
-	print('Temp={0:0.1f}*  Humidity={1:0.1f}% \n'.format(temperature, humidity))
-	sleep(1)
-	data.close()
+    data = open ("data.txt", "a")
+    humidity, temperature = Adafruit_DHT.read_retry(AdafruitDHT.AM2302, 4)
+    data.write('Temp={0:0.1f}*  Humidity={1:0.1f}% \n'.format(temperature, humidity))
+    print('Temp={0:0.1f}*  Humidity={1:0.1f}% \n'.format(temperature, humidity))
+    sleep(1)
+    data.close()
 
